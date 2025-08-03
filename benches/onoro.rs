d4ec465af78c20e19cecd79d86f207498971c8d3
Clayton Knittel <cknit1999@gmail.com>
2025-08-02 20:55:05 -0700

    Find moves phase 1 bench

---
+++ benches/onoro.rs
@@ -0,0 +1,82 @@
+use std::{hint::black_box, time::Duration};
+
+use criterion::{
+  criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup, Criterion, Throughput,
+};
+use itertools::Itertools;
+use onoro::Onoro16;
+use rand::{thread_rng, Rng};
+
+fn random_playout<R: Rng>(mut onoro: Onoro16, num_moves: usize, rng: &mut R) -> Onoro16 {
+  for _ in 0..num_moves {
+    let moves = onoro.each_move().collect_vec();
+    let m = moves[rng.gen_range(0..moves.len())];
+    onoro.make_move(m);
+  }
+  onoro
+}
+
+#[inline(never)]
+fn generate_random_states<R: Rng>(count: usize, num_moves: usize, rng: &mut R) -> Vec<Onoro16> {
+  (0..count)
+    .map(|_| random_playout(Onoro16::default_start(), num_moves, rng))
+    .collect()
+}
+
+fn benchmark_each_move<M: Measurement, R: Rng>(
+  group: &mut BenchmarkGroup<M>,
+  id: &str,
+  num_games: usize,
+  num_moves: usize,
+  rng: &mut R,
+) {
+  let states = generate_random_states(num_games, num_moves, rng);
+  group.bench_function(id, |b| {
+    b.iter(|| {
+      for onoro in states.iter() {
+        for m in onoro.each_move() {
+          black_box(m);
+        }
+      }
+    })
+  });
+}
+
+fn find_moves_p1(c: &mut Criterion) {
+  const N_GAMES: usize = 10_000;
+
+  let mut group = c.benchmark_group("find moves phase 1");
+  group.throughput(Throughput::Elements(N_GAMES as u64));
+  group.measurement_time(Duration::from_secs(20));
+
+  let mut rng = thread_rng();
+
+  benchmark_each_move(
+    &mut group,
+    "find moves phase 1 after 4 moves",
+    N_GAMES,
+    4,
+    &mut rng,
+  );
+
+  benchmark_each_move(
+    &mut group,
+    "find moves phase 1 after 8 moves",
+    N_GAMES,
+    8,
+    &mut rng,
+  );
+
+  benchmark_each_move(
+    &mut group,
+    "find moves phase 1 after 12 moves",
+    N_GAMES,
+    12,
+    &mut rng,
+  );
+
+  group.finish();
+}
+
+criterion_group!(onoro_benches, find_moves_p1);
+criterion_main!(onoro_benches);
