5a2f76126d8917bcb766b496d8b4dbb699a01555
Clayton Knittel <cknit1999@gmail.com>
2025-08-11 00:12:44 -0700

    packed_positions_to_mask with sse

--- onoro_impl/src/util.rs
+++ onoro_impl/src/util.rs
@@ -1,3 +1,8 @@
+use core::arch::x86_64::{__m128i, _mm_set_epi8, _mm_shuffle_epi8};
+use std::arch::x86_64::{_mm_cvtsi128_si64x, _mm_set_epi64x};
+
+use itertools::Itertools;
+
 #[inline]
 pub const fn unreachable() -> ! {
   #[cfg(debug_assertions)]
@@ -40,4 +45,96 @@ define_cmp!(max_i64, min_i64, i64);
 pub const fn broadcast_u8_to_u64(v: u8) -> u64 {
   const BYTE_ANCHOR: u64 = 0x0101_0101_0101_0101;
   (v as u64) * BYTE_ANCHOR
+}
+
+/// Given 8 byte values packed into a u64, returns a u64 with each
+/// corresponding bit index set for each byte (1-indexed). Zero byte values are
+/// ignored. All non-zero bytes must be unique.
+#[target_feature(enable = "ssse3")]
+unsafe fn packed_positions_to_mask_sse3(packed_positions: u64) -> u64 {
+  debug_assert!(
+    packed_positions
+      .to_ne_bytes()
+      .into_iter()
+      .all(|byte| byte < 0x10)
+  );
+  debug_assert!(
+    packed_positions
+      .to_ne_bytes()
+      .into_iter()
+      .filter(|&byte| byte != 0)
+      .all_unique()
+  );
+
+  let lo_data = _mm_set_epi8(
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0x80u8 as i8,
+    0x40,
+    0x20,
+    0x10,
+    0x08,
+    0x04,
+    0x02,
+    0x01,
+    0,
+  );
+  let hi_data = _mm_set_epi8(
+    0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  );
+
+  let shuffle_mask = _mm_set_epi64x(0, packed_positions as i64);
+  let lo_masks = _mm_shuffle_epi8(lo_data, shuffle_mask);
+  let hi_masks = _mm_shuffle_epi8(hi_data, shuffle_mask);
+
+  let lo_masks = _mm_cvtsi128_si64x(lo_masks) as u64;
+  let hi_masks = _mm_cvtsi128_si64x(hi_masks) as u64;
+
+  let lo_masks = (lo_masks | (lo_masks >> 8)) & 0x00ff_00ff_00ff_00ff;
+  let hi_masks = (hi_masks | (hi_masks << 8)) & 0xff00_ff00_ff00_ff00;
+  let masks = lo_masks | hi_masks;
+
+  let masks = masks | (masks >> 16);
+  let masks = masks | (masks >> 32);
+  masks & 0x0000_0000_0000_ffff
+}
+
+#[cfg(any(test, not(target_feature = "ssse3")))]
+fn packed_positions_to_mask_slow(packed_positions: u64) -> u64 {
+  packed_positions
+    .to_ne_bytes()
+    .into_iter()
+    .filter(|&byte| byte != 0)
+    .fold(0, |mask, byte| mask | (1u64 << (byte - 1)))
+}
+
+pub fn packed_positions_to_mask(packed_positions: u64) -> u64 {
+  #[cfg(target_feature = "ssse3")]
+  unsafe {
+    packed_positions_to_mask_sse3(packed_positions)
+  }
+  #[cfg(not(target_feature = "ssse3"))]
+  packed_positions_to_mask_slow(packed_positions)
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::util::{packed_positions_to_mask, packed_positions_to_mask_slow};
+
+  #[test]
+  fn test_packed_positions_to_mask() {
+    assert_eq!(packed_positions_to_mask(0x0102030405060708), 0xff);
+    assert_eq!(packed_positions_to_mask(0x01040506070a0c0e), 0x2a79);
+  }
+
+  #[test]
+  fn test_packed_positions_to_mask_slow() {
+    assert_eq!(packed_positions_to_mask_slow(0x0102030405060708), 0xff);
+    assert_eq!(packed_positions_to_mask_slow(0x01040506070a0c0e), 0x2a79);
+  }
 }
