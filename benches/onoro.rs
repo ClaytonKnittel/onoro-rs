bfa5cc192e72f082d03f8e252774690d389dc028
Clayton Knittel <cknit1999@gmail.com>
2025-08-21 10:26:10 -0700

    Onoro view benchmark

--- benches/onoro.rs
+++ benches/onoro.rs
@@ -3,116 +3,14 @@ use std::{hint::black_box, time::Duration};
 use criterion::{
   criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup, Criterion, Throughput,
 };
-use itertools::Itertools;
-use onoro::{
-  error::{OnoroError, OnoroResult},
-  Onoro, OnoroPawn,
+use onoro::{error::OnoroResult, Onoro, OnoroPawn};
+use onoro_impl::{
+  benchmark_util::{
+    generate_random_unfinished_states, generate_random_walks, random_playout, CheckWinBenchmark,
+  },
+  Onoro16,
 };
-use onoro_impl::{benchmark_util::CheckWinBenchmark, Move, Onoro16};
 use rand::{rngs::StdRng, Rng, SeedableRng};
-
-fn make_random_move<R: Rng>(onoro: &mut Onoro16, rng: &mut R) -> Move {
-  let mut moves = onoro.each_move().collect_vec();
-  moves.sort_by(|&m1, &m2| match (m1, m2) {
-    (Move::Phase1Move { to: to1 }, Move::Phase1Move { to: to2 }) => {
-      (to1.x(), to1.y()).cmp(&(to2.x(), to2.y()))
-    }
-
-    (
-      Move::Phase2Move {
-        to: to1,
-        from_idx: from1,
-      },
-      Move::Phase2Move {
-        to: to2,
-        from_idx: from2,
-      },
-    ) => (to1.x(), to1.y())
-      .cmp(&(to2.x(), to2.y()))
-      .then(from1.cmp(&from2)),
-
-    // All moves should be in the same phase.
-    _ => unreachable!(),
-  });
-  assert!(
-    !moves.is_empty(),
-    "No moves available in position:\n{onoro:?}"
-  );
-  let m = moves[rng.gen_range(0..moves.len())];
-  onoro.make_move(m);
-  m
-}
-
-/// Plays a random number of moves in the game, returning the number of moves
-/// played until the game finished. If the game did not finish, returns
-/// `num_moves + 1`.
-fn random_playout<R: Rng>(onoro: &mut Onoro16, num_moves: usize, rng: &mut R) -> usize {
-  for i in 1..=num_moves {
-    make_random_move(onoro, rng);
-    if onoro.finished().is_some() {
-      return i;
-    }
-  }
-
-  num_moves + 1
-}
-
-#[inline(never)]
-fn generate_random_unfinished_states<R: Rng>(
-  count: usize,
-  num_moves: usize,
-  rng: &mut R,
-) -> OnoroResult<Vec<Onoro16>> {
-  let mut states = Vec::with_capacity(count);
-
-  let attempts = 100 * count;
-  for _ in 0..attempts {
-    let mut onoro = Onoro16::default_start();
-    if random_playout(&mut onoro, num_moves, rng) > num_moves {
-      states.push(onoro);
-    }
-    if states.len() == count {
-      return Ok(states);
-    }
-  }
-
-  Err(
-    OnoroError::new(format!(
-      "Failed to generate {count} random states with {num_moves} moves after {attempts} attempts"
-    ))
-    .into(),
-  )
-}
-
-#[inline(never)]
-fn generate_random_walks<R: Rng>(
-  initial_state: &Onoro16,
-  count: usize,
-  rng: &mut R,
-) -> OnoroResult<Vec<Vec<Move>>> {
-  const MAX_MOVES: usize = 1000;
-  debug_assert!(initial_state.finished().is_none());
-
-  (0..count)
-    .map(|_| {
-      let mut onoro = initial_state.clone();
-      let mut moves = Vec::new();
-      for _ in 0..MAX_MOVES {
-        if onoro.finished().is_some() {
-          return Ok(moves);
-        }
-        moves.push(make_random_move(&mut onoro, rng));
-      }
-
-      Err(
-        OnoroError::new(format!(
-          "Exceeded maximum number of moves {MAX_MOVES} without finishing the game."
-        ))
-        .into(),
-      )
-    })
-    .collect()
-}
 
 fn benchmark_each_move<M: Measurement, R: Rng>(
   group: &mut BenchmarkGroup<M>,
---
+++ benches/onoro_view.rs
@@ -0,0 +1,43 @@
+use std::{hint::black_box, time::Duration};
+
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use onoro_impl::{benchmark_util::generate_random_unfinished_states, OnoroView};
+use rand::{rngs::StdRng, SeedableRng};
+
+fn construct_views(c: &mut Criterion) {
+  const N_GAMES: usize = 10_000;
+
+  let mut group = c.benchmark_group("construct");
+  group.throughput(Throughput::Elements(N_GAMES as u64));
+  group.measurement_time(Duration::from_secs(20));
+
+  let mut rng = StdRng::seed_from_u64(90383240);
+  let states = generate_random_unfinished_states(N_GAMES, 18, &mut rng).unwrap();
+
+  #[cfg(feature = "profiled")]
+  let guard = pprof::ProfilerGuardBuilder::default()
+    .frequency(1000)
+    .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+    .build()
+    .unwrap();
+
+  group.bench_function("make move", |b| {
+    b.iter(|| {
+      for onoro in &states {
+        let view = OnoroView::find_canonical_view(onoro);
+        black_box(view);
+      }
+    })
+  });
+
+  #[cfg(feature = "profiled")]
+  if let Ok(report) = guard.report().build() {
+    let file = std::fs::File::create("onoro_view_construct.svg").unwrap();
+    report.flamegraph(file).unwrap();
+  };
+
+  group.finish();
+}
+
+criterion_group!(onoro_view_benches, construct_views);
+criterion_main!(onoro_view_benches);
