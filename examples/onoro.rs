7c22eea33450d8425a6afea5ad926f4ac74e92e3
Clayton Knittel <cknit1999@gmail.com>
2025-09-14 20:50:52 -0700

    Use iterative deepening solver

--- examples/onoro.rs
+++ examples/onoro.rs
@@ -1,4 +1,4 @@
-use cooperate::solvers::ttable_alpha_beta::TTAlphaBeta;
+use cooperate::solvers::iter_deep::IterativeDeepening;
 use onoro::{
   abstract_game::interactive::{
     bot_player::BotPlayer, human_term_player::HumanTermPlayer, term_interface::TermInterface,
@@ -11,7 +11,7 @@ use onoro_impl::{solver::OnoroSolver, Onoro16};
 fn main() {
   let player1 = HumanTermPlayer::new("Player 1".to_owned(), OnoroPlayer::new());
 
-  let solver = OnoroSolver::new(TTAlphaBeta::new());
+  let solver = OnoroSolver::new(IterativeDeepening::new());
   let player2 = BotPlayer::new("Player 2".to_owned(), solver, 10);
 
   let game = Onoro16::default_start();
